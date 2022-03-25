818698c12f932aef283c12874fd8f321f2b41f8a
adrian valencia hernandez <adrian15996@gmail.com>
2022-03-25 13:19:52 -0600

    primer Usestate

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import '@styles/ProductItem.scss';
 
 const ProductItem = () => {
-	return (
-		const [cart, setCart] = useState([]);
+	const [cart, setCart] = useState('');
 		const handleCart = () =>{
-			setCart
+			setCart('hello world');
 		}
+	return (
 		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
 			<div className="product-info">
@@ -14,9 +14,10 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure onClick={}>
+				<figure onClick={handleCart}>
 					<img src="./icons/bt_add_to_cart.svg" alt="" />
 				</figure>
+				{cart}
 			</div>
 		</div>
 	);
